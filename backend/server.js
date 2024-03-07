5d6d33c378de7f752c1dda3625dab1b40aa067ca
Divyanshu Pandey <pandeydivyanshu257@gmail.com>
2024-03-07 19:58:55 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -8,6 +9,7 @@ import userRoutes from "./routes/users.routes.js";
 import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,7 +24,13 @@ app.use("/api/users", userRoutes);
 
 // app.get("/api/auth/signup", (req, res) => {
 //   console.log("signup route");
-// });
+// });'
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
